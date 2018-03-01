bb67d0052700dcfb20e8fd380611fffb2434be9b
Алексей Шкарупа <shkarupa.alex@gmail.com>
2018-03-01 11:27:35 +0300

    Обновлен webpack и зависимости

--- index.js
+++ index.js
@@ -7,18 +7,20 @@ function HtmlWebpackAssetTemplatePlugin(options) {
 }
 
 HtmlWebpackAssetTemplatePlugin.prototype.apply = function(compiler) {
-  compiler.plugin('compilation', function(compilation) {
-    compilation.plugin('html-webpack-plugin-before-html-processing',
-      (htmlPluginData, callback) => {
-        const filename = htmlPluginData.plugin.options.filename;
-        if (compilation.assets[filename]) {
-          htmlPluginData.html = compilation.assets[filename].source();
-          delete compilation.assets[filename];
-        }
+  compiler.hooks.compilation.tap('HtmlWebpackAssetTemplatePlugin',
+    function(compilation) {
+      compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing.tapAsync(
+        'HtmlIndexPlugin',
+        function(htmlPluginData, callback) {
+          const filename = htmlPluginData.plugin.options.filename;
+          if (compilation.assets[filename]) {
+            htmlPluginData.html = compilation.assets[filename].source();
+            delete compilation.assets[filename];
+          }
 
-        callback(null, htmlPluginData);
-      });
-  });
+          callback(null, htmlPluginData);
+        });
+    });
 };
 
 module.exports = HtmlWebpackAssetTemplatePlugin;
